c9010e15d79b7cc40c69a32f211a44a7002dc047
Henrik Claßen <henrik.classen@web.de>
2024-11-20 22:11:00 +0100

    mcunet: tinynas: oneshot: Add subproject
    This subproject contains the "supernet" definition as well as
    methods for crossover and mutation used in evolution search.
    
    Signed-off-by: Henrik Claßen <henrik.classen@web.de>

---
+++ src/mcunet/tinynas/oneshot/helper.py
@@ -0,0 +1,258 @@
+from bisect import insort_left
+
+import torch
+import torch.nn as nn
+import torch.linalg as la
+
+from torchvision.ops import SqueezeExcitation
+
+from ..searchspace.layers import (
+    LayerName,
+    BaseModule,
+    Conv2dModule,
+    DWConv2dModule,
+    BDWRConv2dModule
+)
+
+
+__all__ = ["l1_sort", "has_norm", "reduce_channels"]
+
+
+def l1_sort(tensor: torch.Tensor) -> tuple[torch.Tensor, tuple[int, ...]]:
+    """
+    Sorts the values of the tensor `weight` by taking the L1 norm of the entries
+    of its first dimension. The higher the result, the more important is the
+    entry.
+
+    Args:
+        tensor (torch.Tensor):
+            The input tensor
+
+    Returns:
+        tuple[torch.Tensor, tuple[int, ...]]:
+            A new tensor with the sorted entries of `tensor` and a tuple with
+            the indices of sorted entries.
+    """
+    importance: list[tuple[int, float]] = []
+
+    for channel in range(tensor.shape[0]):
+        l1 = la.norm(tensor[channel].view(-1), ord=1)
+        insort_left(
+            importance, (channel, l1), key=lambda entry: -entry[1]
+        )
+
+    reordered = torch.empty_like(tensor, requires_grad=tensor.requires_grad)
+    with torch.no_grad():
+        for k, (channel, _) in enumerate(importance):
+            reordered[k].copy_(tensor[channel])
+
+    return reordered, tuple(map(lambda x: x[0], importance))
+
+
+def _copy_weights_conv2d(
+    dst: nn.Conv2d, src: nn.Conv2d, in_channels: int, out_channels: int
+) -> None:
+    dst.weight.copy_(src.weight[:out_channels, :in_channels])
+    if src.bias is not None and dst.bias is not None:
+        dst.bias.copy_(src.bias[:out_channels])
+
+
+def _copy_weights_norm(
+    dst: nn.BatchNorm2d, src: nn.BatchNorm2d, out_channels: int
+) -> None:
+    dst.weight.copy_(src.weight[:out_channels])
+    dst.bias.copy_(src.bias[:out_channels])
+    dst.running_mean.copy_(src.running_mean[:out_channels])
+    dst.running_var.copy_(src.running_var[:out_channels])
+    dst.num_batches_tracked = src.num_batches_tracked
+
+
+def _copy_weights_squeeze(
+    dst: SqueezeExcitation, src: SqueezeExcitation, in_channels: int
+) -> None:
+    out_channels = dst.fc1.weight.shape[0]
+
+    _copy_weights_conv2d(dst.fc1, src.fc1, in_channels, out_channels)
+    _copy_weights_conv2d(dst.fc2, src.fc2, out_channels, in_channels)
+
+
+def has_norm(sequential: nn.Sequential) -> bool:
+    """
+    Checks if the second module in `sequential` is a a `BatchNorm2d` layer.
+
+    Args:
+        sequential (torch.nn.Sequential):
+            The module to check.
+
+    Returns:
+        bool:
+            `True`, if the scond module is `BatchNorm2d` layer.
+    """
+    return len(sequential) > 1 and isinstance(sequential[1], nn.BatchNorm2d)
+
+
+def _reduce_conv2d(
+    module: Conv2dModule, in_channels: int, out_channels: int
+) -> Conv2dModule:
+    reduced = Conv2dModule(
+        in_channels,
+        out_channels,
+        module.se_ratio,
+        module.shortcut,
+        module[LayerName.CONV2D][0].kernel_size,
+        module[LayerName.CONV2D][0].stride,
+    )
+
+    with torch.no_grad():
+        _copy_weights_conv2d(
+            reduced[LayerName.CONV2D][0],
+            module[LayerName.CONV2D][0],
+            in_channels, out_channels
+        )
+        if has_norm(reduced[LayerName.CONV2D]):
+            _copy_weights_norm(
+                reduced[LayerName.CONV2D][1],
+                module[LayerName.CONV2D][1],
+                out_channels
+            )
+
+        if LayerName.SE in reduced:
+            _copy_weights_squeeze(
+                reduced[LayerName.SE], module[LayerName.SE], out_channels
+            )
+
+    return reduced
+
+
+def _reduce_dwconv2d(
+    module: DWConv2dModule, in_channels: int, out_channels: int
+) -> Conv2dModule:
+    reduced = DWConv2dModule(
+        in_channels,
+        out_channels,
+        module.se_ratio,
+        module.shortcut,
+        module[LayerName.CONV2D][0].kernel_size,
+        module[LayerName.CONV2D][0].stride,
+    )
+
+    with torch.no_grad():
+        _copy_weights_conv2d(
+            reduced[LayerName.CONV2D][0], module[LayerName.CONV2D][0],
+            in_channels, in_channels
+        )
+        if has_norm(reduced[LayerName.CONV2D]):
+            _copy_weights_norm(
+                reduced[LayerName.CONV2D][1],
+                module[LayerName.CONV2D][1],
+                in_channels
+            )
+
+        if LayerName.SE in reduced:
+            _copy_weights_squeeze(
+                reduced[LayerName.SE], module[LayerName.SE], in_channels
+            )
+
+        _copy_weights_conv2d(
+            reduced[LayerName.PWCONV2D][0], module[LayerName.PWCONV2D][0],
+            in_channels, out_channels
+        )
+
+        if has_norm(reduced[LayerName.PWCONV2D]):
+            _copy_weights_norm(
+                reduced[LayerName.PWCONV2D][1],
+                module[LayerName.PWCONV2D][1],
+                out_channels
+            )
+
+    return reduced
+
+
+def _reduce_bdwrconv2d(
+    module: BDWRConv2dModule, in_channels: int, out_channels: int
+) -> Conv2dModule:
+    reduced = BDWRConv2dModule(
+        in_channels,
+        out_channels,
+        module.expansion_ratio,
+        module.se_ratio,
+        module.shortcut,
+        module[LayerName.CONV2D][0].kernel_size,
+        module[LayerName.CONV2D][0].stride,
+    )
+
+    with torch.no_grad():
+        hidden = in_channels * module.expansion_ratio
+        _copy_weights_conv2d(
+            reduced[LayerName.EXPANSION][0],
+            module[LayerName.EXPANSION][0],
+            in_channels, hidden
+        )
+        if has_norm(reduced[LayerName.EXPANSION]):
+            _copy_weights_norm(
+                reduced[LayerName.EXPANSION][1],
+                module[LayerName.EXPANSION][1],
+                hidden
+            )
+
+        _copy_weights_conv2d(
+            reduced[LayerName.CONV2D][0],
+            module[LayerName.CONV2D][0],
+            hidden, hidden
+        )
+        if has_norm(reduced[LayerName.CONV2D]):
+            _copy_weights_norm(
+                reduced[LayerName.CONV2D][1], module[LayerName.CONV2D][1], hidden
+            )
+
+        if LayerName.SE in reduced:
+            _copy_weights_squeeze(
+                reduced[LayerName.SE], module[LayerName.SE], hidden
+            )
+
+        _copy_weights_conv2d(
+            reduced[LayerName.PWCONV2D][0],
+            module[LayerName.PWCONV2D][0],
+            hidden, out_channels
+        )
+        if has_norm(reduced[LayerName.PWCONV2D]):
+            _copy_weights_norm(
+                reduced[LayerName.PWCONV2D][1],
+                module[LayerName.PWCONV2D][1],
+                out_channels
+            )
+
+    return reduced
+
+
+def reduce_channels(
+    module: BaseModule, in_channels: int, out_channels: int
+) -> BaseModule:
+    """
+    Produces a copy of `module` with the input and output channels reduces to
+    `in_channels` and `out_channels`. Copies all the weights cut off at the new
+    channel size.
+
+    Args:
+        module (BaseModule):
+            The module to reduce.
+        in_channels (int):
+            The new and reduces number of input channels.
+        out_channels (int):
+            The new and reduces number of output channels.
+
+    Returns:
+        BaseModule:
+            A copy of `module` with the input ad output channels reduced.
+    """
+    reducer = {
+        Conv2dModule: _reduce_conv2d,
+        DWConv2dModule: _reduce_dwconv2d,
+        BDWRConv2dModule: _reduce_bdwrconv2d
+    }
+
+    fn = reducer.get(module.__class__)
+    if fn is None:
+        raise RuntimeError(f"unknown operation {type(module)}")
+
+    return fn(module, in_channels, out_channels)
---
+++ src/mcunet/tinynas/oneshot/share.py
@@ -0,0 +1,331 @@
+from typing import Annotated
+from typing_extensions import Doc
+from abc import ABC, abstractmethod
+from collections.abc import Callable
+
+import torch
+import torch.nn as nn
+
+from .helper import l1_sort, has_norm
+
+
+__all__ = [
+    "share_conv2d",
+    "share_batchnorm2d",
+    "make_shared_conv2d_batchnorm2d",
+    "init_conv2d",
+    "init_conv2d_batchnorm2d",
+    "set_conv2d",
+    "set_conv2d_batchnorm2d",
+    "ParameterSharer",
+    "ParameterSharerConstructor"
+]
+
+
+def share_conv2d(
+    conv2d: nn.Conv2d,
+    weight: nn.Parameter,
+    bias: nn.Parameter | None
+) -> None:
+    """
+    Set the weight and bias of `conv2d` to shared parameters. The bias is only
+    shared if `bias is not None and conv2d.bias is not None`.
+
+    The size of `weight` and `bias` must be equal or greater than required by
+    `conv2d`.
+
+    With c_in, c_out as the in and out channels of `conv2d` and h, w the height
+    and width of the kernel, the sharing is applied as
+    `conv2d.weight = weight[:c_out, :c_in, h - center:h + center, w - center:c + center]`
+    and
+    `conv2d.bias = bias[c:out:]`.
+
+    Args:
+        conv2d (nn.Conv2d):
+            The convolution module.
+        weight (nn.Parameter):
+            The shared weight.
+        bias (nn.Parameter, None):
+            The shared bias.
+    """
+    _, _, weight_height, weight_width = weight.size()
+
+    weight_height_center = int((weight_height - 1) / 2)
+    weight_width_center = int((weight_width - 1) / 2)
+
+    if isinstance(conv2d.kernel_size, tuple):
+        kernel_height, kernel_width = conv2d.kernel_size
+    else:
+        kernel_height, kernel_width = conv2d.kernel_size, conv2d.kernel_size
+
+    height_center = int((kernel_height - 1) / 2)
+    width_center = int((kernel_width - 1) / 2)
+
+    h_start = weight_height_center - height_center
+    h_end = weight_height_center + height_center + 1
+
+    w_start = weight_width_center - width_center
+    w_end = weight_width_center + width_center + 1
+
+    out_channels = conv2d.out_channels
+    in_channels = int(conv2d.in_channels / conv2d.groups)
+
+    conv2d.weight = nn.Parameter(
+        weight[:out_channels, :in_channels, h_start:h_end, w_start:w_end]
+    )
+
+    if conv2d.bias is None or bias is None:
+        return
+
+    conv2d.bias = nn.Parameter(bias[:out_channels])
+
+
+def share_batchnorm2d(
+    norm: nn.BatchNorm2d,
+    weight: nn.Parameter,
+    bias: nn.Parameter,
+    running_mean: torch.Tensor | None,
+    running_var: torch.Tensor | None,
+    num_batches_tracked: torch.Tensor | None
+) -> None:
+    """
+    Set the weight, bias, running mean, running variance and number of tracked
+    batches of `norm` to shared parameters. The running mean, running variance
+    and number of tracked batches are optional.
+
+    The sizes of the parameters must be equal or greater than required by `norm`.
+
+    The parameters are index by `[:num_features]`, except for
+    `num_batches_tracked`.
+
+    Args:
+        norm (nn.BatchNorm2d):
+            The batch normalization module.
+        weight (nn.Parameter):
+            The shared weight.
+        bias (nn.Parameter):
+            The shared bias.
+        running_mean (torch.Tensor, None):
+            The shared running mean.
+        running_var (torch.Tensor, None):
+            The shared running variance.
+        num_batches_tracked (torch.Tensor, None):
+            The shared number of tracked batches.
+    """
+    num_features = norm.num_features
+    norm.weight = nn.Parameter(weight[:num_features])
+    norm.bias = nn.Parameter(bias[:num_features])
+
+    if running_mean is not None:
+        norm.running_mean = running_mean[:num_features]
+
+    if running_var is not None:
+        norm.running_var = running_var[:num_features]
+
+    if num_batches_tracked is not None:
+        norm.num_batches_tracked = num_batches_tracked
+
+
+def make_shared_conv2d(
+    out_channels: int,
+    in_channels: int,
+    kernel_size: tuple[int, int]
+) -> tuple[nn.Parameter, nn.Parameter]:
+    """
+    Creates the two shared parameters of a conv2d.
+
+    Args:
+        out_channels (int):
+            The out channels of the two layers.
+        in_channels (int):
+            The in channels of the two layers.
+        kernel_size (tuple[int, int]):
+            The kernel size of the conv2d.
+
+    Returns:
+        tuple[torch.nn.Parameter, torch.nn.Parameter]:
+            The shared weight and bias of a conv2d.
+    """
+    conv2d_weight = nn.Parameter(
+        torch.Tensor(out_channels, in_channels, kernel_size[0], kernel_size[1])
+    )
+    bias = nn.Parameter(torch.Tensor(out_channels))
+
+    return conv2d_weight, bias
+
+
+def make_shared_conv2d_batchnorm2d(
+    out_channels: int,
+    in_channels: int,
+    kernel_size: tuple[int, int]
+) -> tuple[nn.Parameter, nn.Parameter, nn.Parameter]:
+    """
+    Creates the three shared parameters for a conv2d followed by a
+    batch-normalisation.
+
+    Args:
+        out_channels (int):
+            The out channels of the two layers.
+        in_channels (int):
+            The in channels of the two layers.
+        kernel_size (tuple[int, int]):
+            The kernel size of the conv2d.
+
+    Returns:
+        tuple[torch.nn.Parameter, torch.nn.Parameter, torch.nn.Parameter]:
+            The shared weight of the conv2d, the shared weight and bias of the
+            batch-normalisation.
+    """
+    conv2d_weight = nn.Parameter(
+        torch.Tensor(out_channels, in_channels, kernel_size[0], kernel_size[1])
+    )
+    norm_weight = nn.Parameter(torch.Tensor(out_channels))
+    norm_bias = nn.Parameter(torch.Tensor(out_channels))
+
+    return conv2d_weight, norm_weight, norm_bias
+
+
+def init_conv2d(weight: nn.Parameter, bias: nn.Parameter | None) -> None:
+    """
+    Initializes the two shared parameters of a conv2d.
+
+    Args:
+        weight (torch.nn.Parameter):
+            The shared weight of the conv2d.
+        bias (torch.nn.Parameter, None):
+            The shared bias of the conv2d.
+    """
+    nn.init.kaiming_normal_(weight, mode="fan_out")
+    if bias is not None:
+        nn.init.zeros_(bias)
+
+
+def init_conv2d_batchnorm2d(
+    weight: nn.Parameter, norm_weight: nn.Parameter, norm_bias: nn.Parameter
+) -> None:
+    """
+    Initializes the three shared parameters of a conv2d followed by a
+    batch-normalisation.
+
+    Args:
+        weight (torch.nn.Parameter):
+            The shared weight of the conv2d.
+        norm_weight (torch.nn.Parameter):
+            The shared weight of the batch-normalisation.
+        norm_bias (torch.nn.Parameter):
+            The shared bias of the batch-normalisation.
+    """
+    nn.init.kaiming_normal_(weight, mode="fan_out")
+    nn.init.ones_(norm_weight)
+    nn.init.zeros_(norm_bias)
+
+
+def set_conv2d(
+    weight: nn.Parameter,
+    bias: nn.Parameter | None,
+    conv2d: nn.Conv2d
+) -> None:
+    """
+    Set the two shared parameters of a conv2d to the weights in
+    `conv2d`.
+
+    Args:
+        weight (torch.nn.Parameter):
+            The shared weight of the conv2d.
+        bias (torch.nn.Parameter, None):
+            The shared bias of the conv2d.
+        conv2d (torch.nn.Conv2d):
+            The layers to copy the weights from.
+    """
+    src_weight = conv2d.weight
+    reordered, importance = l1_sort(src_weight)
+
+    with torch.no_grad():
+        weight.copy_(reordered)
+
+        if conv2d.bias is None or bias is None:
+            return
+
+        src_bias = conv2d.bias
+        for k, channel in enumerate(importance):
+            bias[k] = src_bias[channel]
+
+
+def set_conv2d_batchnorm2d(
+    weight: nn.Parameter,
+    norm_weight: nn.Parameter,
+    norm_bias: nn.Parameter,
+    sequential: nn.Sequential
+) -> None:
+    """
+    Set the three shared parameters of a conv2d followed by a
+    batch-normalisation to the weights in `sequential`.
+
+    Args:
+        weight (torch.nn.Parameter):
+            The shared weight of the conv2d.
+        norm_weight (torch.nn.Parameter):
+            The shared weight of the batch-normalisation.
+        norm_bias (torch.nn.Parameter):
+            The shared bias of the batch-normalisation.
+        sequential (torch.nn.Sequential):
+            The layers to copy the weights from.
+    """
+    src_weight = sequential[0].weight
+    reordered, importance = l1_sort(src_weight)
+
+    with torch.no_grad():
+        weight.copy_(reordered)
+
+        if not has_norm(sequential):
+            return
+
+        src_norm_weight = sequential[1].weight
+        src_norm_bias = sequential[1].bias
+        for k, channel in enumerate(importance):
+            norm_weight[k] = src_norm_weight[channel]
+            norm_bias[k] = src_norm_bias[channel]
+
+
+class ParameterSharer(ABC, nn.Module):
+    def __init__(self, max_in_channels: int, max_out_channels: int) -> None:
+        super().__init__()
+
+        self._make_shared(max_in_channels, max_out_channels)
+
+    @abstractmethod
+    def _make_shared(self, max_in_channels: int, max_out_channels: int) -> None:
+        pass
+
+    @abstractmethod
+    def set_shared(self, module: nn.Module) -> None:
+        pass
+
+    @abstractmethod
+    def unset_shared(self, module: nn.Module) -> None:
+        pass
+
+    @abstractmethod
+    def _weight_initialization(self) -> None:
+        pass
+
+
+type ParameterSharerConstructor = Annotated[
+    Callable[[int, int], ParameterSharer],
+    Doc(
+        """
+        Creates an instance of `ParameterSharer`. This just a concrete type for
+        the `ParameterSharer` constructor used for type annotations.
+
+        Args:
+            max_in_channels (int):
+                The maximum number of input channels for this layer.
+            max_out_channels (int):
+                The maximum number of output channels for this layer.
+
+        Returns:
+            ParameterSharer:
+                An instance of a `ParameterSharer` sub-class.
+        """
+    )
+]

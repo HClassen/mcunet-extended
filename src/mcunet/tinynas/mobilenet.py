2e26462cee9edfdbdf3a0bdc32ebdbfcad373793
Henrik Claßen <henrik.classen@web.de>
2024-12-15 20:43:23 +0100

    fixup: mobilenet

--- src/mcunet/tinynas/mobilenet.py
+++ src/mcunet/tinynas/mobilenet.py
@@ -1,13 +1,18 @@
+import time
 from typing import Final
 from functools import cache
 from collections.abc import Callable, Iterable
 
 import torch
 import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
 
 from torchvision.ops import Conv2dNormActivation
 
 from torchprofile import profile_macs
+
+from .utils import make_caption
 
 
 __all__ = [
@@ -178,8 +183,7 @@ class MobileSkeletonNet(nn.Module):
     def _weight_initialization(self) -> None:
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                if m.weight is not None:
-                    nn.init.kaiming_normal_(m.weight, mode="fan_out")
+                nn.init.kaiming_normal_(m.weight, mode="fan_out")
                 if m.bias is not None:
                     nn.init.zeros_(m.bias)
             elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
@@ -200,15 +204,136 @@ class MobileSkeletonNet(nn.Module):
 
         return self.classifier(inputs)
 
+    def run_train(
+        self,
+        ds: Dataset,
+        epochs: int,
+        batch_size: int,
+        momentum: float = 0.9,
+        weight_decay: float = 5e-5,
+        *,
+        batches: int | None = None,
+        device=None
+    ) -> None:
+        """
+        Train the network on the data set `ds` using the `SGD` optimizer.
+
+        Args:
+            ds (Dataset):
+                The data set to train on.
+            epochs (int):
+                The number of training epochs.
+            batch_size (int):
+                The number of samples per batch.
+            momentum (float):
+                The momenum for `SGD` to use.
+            weight_decay (float):
+                The weight decay for `SGD` to use.
+            batches (int, None):
+                The number of batches per epoch. If set to `None` use the whole
+                training data set.
+        """
+        train_start = time.time()
+
+        dl = DataLoader(ds, batch_size, shuffle=True)
+        batches = batches if batches is not None else len(dl)
+
+        self.to(device)
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.SGD(
+            self.parameters(), momentum=momentum, weight_decay=weight_decay
+        )
+
+        self.train()
+        for i in range(epochs):
+            print(make_caption(f"Epoch {i + 1}/{epochs}", 70, "-"))
+            epoch_start = time.time()
+
+            for j, (images, labels) in enumerate(dl):
+                print(f"epoch={i + 1}, batch={j + 1:0{len(str(batches))}}/{batches}", end="")
+                batch_start = time.time()
+
+                images = images.to(device)
+                labels = labels.to(device)
+
+                outputs = self(images)
+                loss = criterion(outputs, labels)
+
+                optimizer.zero_grad()
+
+                loss.backward()
+                optimizer.step()
+
+                batch_time = time.time() - batch_start
+
+                print(f", time={batch_time:.2f}s")
+
+                if batches == j + 1:
+                    break
+
+            epoch_time = time.time() - epoch_start
+            print(f"\ntime={epoch_time:.2f}s\n")
+
+        train_time = time.time() - train_start
+        print(f"\ntotal={train_time:.2f}s\n")
+
+
+    def run_valid(
+        self,
+        ds: Dataset,
+        batch_size: int,
+        *,
+        batches: int | None = None,
+        device=None
+    ) -> float:
+        """
+        Validate the network on the data set `ds`.
+
+        Args:
+            ds (Dataset):
+                The data set to validate on.
+            batch_size (int):
+                The number of samples per batch.
+            batches (int, None):
+                The number of batches per epoch. If set to `None` use the whole
+                training data set.
+
+        Returns:
+            float:
+                The accuracy in [0, 1].
+        """
+        dl = DataLoader(ds, batch_size, shuffle=False)
+        batches = batches if batches is not None else len(dl)
+
+        self.to(device)
+        self.eval()
+
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for i, (features, labels) in enumerate(dl):
+                features = features.to(device)
+                labels = labels.to(device)
+
+                outputs = self(features)
+                _, predicted = torch.max(outputs.data, 1)
+                correct += (predicted == labels).sum().item()
+                total += labels.size(0)
+
+                if batches == i + 1:
+                    break
+
+        return float(correct) / float(total)
+
     @cache
-    def flops(self, resolution: int, device=None) -> int:
+    def flops(self, resolution: int, *, device=None) -> int:
         """
         The FLOPs/MACs of this model. They are calculated once and the result is
         cached for future uses.
 
         Args:
-            device:
-                If present move ``self`` and tensor to ``device``.
+            resolution (int):
+                The resolution of the input image.
 
         Returns:
             int:

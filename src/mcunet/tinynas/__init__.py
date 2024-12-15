2b9c15ee976cad10daffbe9a5ff24e9c2e875773
Henrik Claßen <henrik.classen@web.de>
2024-12-15 20:57:13 +0100

    fixup: sample manager

--- src/mcunet/tinynas/__init__.py
+++ src/mcunet/tinynas/__init__.py
@@ -10,8 +10,7 @@ from torch.utils.data import Dataset, DataLoader, random_split
 
 import numpy as np
 
-from .searchspace import configurations, Model, SearchSpace
-from .searchspace.model import build_model
+from .searchspace import Model, SearchSpace
 from .oneshot import (
     initial_population,
     crossover,
@@ -20,8 +19,7 @@ from .oneshot import (
     SuperNet
 )
 from .datasets import CustomDataset
-from .utils import make_caption, EDF
-from .utils.torchhelper import get_device
+from .utils import make_caption
 
 
 __all__ = ["SearchSpace", "SampleManager"]
@@ -35,49 +33,20 @@ class SampleManager():
     then continued.
     """
     _spaces: list[SearchSpace]
-    _classes: int
-    _models: list[list[searchspace.Model]] | None
-    _flops: list[list[int]] | None
-    _edfs: list[EDF] | None
+    _models: list[list[Model]] | None
+    _measurements: list[list[tuple[Any, ...]]] | None
     _index: tuple[int, int] | None
 
-    def __init__(
-        self,
-        classes: int,
-        spaces: Iterable[SearchSpace] | Iterable[tuple[float, int]] | None = None
-    ) -> None:
-        if spaces is None:
-            spaces = configurations()
-
-        spaces = list(spaces)
-        if isinstance(spaces[0], tuple):
-            spaces = [SearchSpace(x, y) for x, y in spaces]
-
+    def __init__(self, spaces: list[SearchSpace]) -> None:
         self._spaces = spaces
-        self._classes = classes
 
         self._models = None
-        self._flops = None
-        self._edfs = None
+        self._measurements = None
         self._index = None
-
-    @property
-    def edfs(self) -> list[EDF]:
-        """
-        Get the list of all EDFs for all FLOPs of sampled models per search
-        space.
-
-        Returns:
-            list[EDF]:
-                A list of ``EDF`` objects.
-        """
-        self.update_edfs()
-
-        return self._edfs
 
     def sample(self, m: int = 1000) -> None:
         """
-        Sample ``m`` models from the MnasNet search space per configuration. It
+        Sample `m` models from the MnasNet search space per configuration. It
         only generate architecture descriptions and not any Pytorch models.
 
         Args:
@@ -86,25 +55,32 @@ class SampleManager():
         """
         self._models = [list(islice(space, m)) for space in self._spaces]
 
-    def _append_flops(self, i: int, j: int) -> None:
+    def _append_measurement(
+        self, fn: Callable[[Model, float, int], tuple[Any, ...]], i: int, j: int
+    ) -> None:
+        model = self._models[i][j]
+        width_mult = self._spaces[i].width_mult
         resolution = self._spaces[i].resolution
-        net = build_model(self._models[i][j], self._classes)
 
-        self._flops[i].append(net.flops(resolution, get_device()))
+        self._measurements[i].append(fn(model, width_mult, resolution))
 
-    def flops(self, batch: int | None = None) -> None:
+    def measure(
+        self,
+        fn: Callable[[Model, float, int], tuple[Any, ...]],
+        batch: int | None = None
+    ) -> None:
         """
-        Computes the FLOPs for the models generated via ``sample()``. This
+        Computes the FLOPs for the models generated via `sample()`. This
         method can be called several times until all models have their FLOPs
         computed.
 
         Args:
             batch (int, None):
                 The amount of models for which the FLOPs should be computed. If
-                ``None`` compute them for all models.
+                `None` compute them for all models.
         """
-        if self._flops is None:
-            self._flops = []
+        if self._measurements is None:
+            self._measurements = []
 
         if self._index is None:
             self._index = (0, 0)
@@ -120,11 +96,11 @@ class SampleManager():
 
         move = idx1 + batch
 
-        if idx0 == len(self._flops):
-            self._flops.append([])
+        if idx0 == len(self._measurements):
+            self._measurements.append([])
 
         for j in range(idx1, min(move, n_samples)):
-            self._append_flops(idx0, j)
+            self._append_measurement(fn, idx0, j)
 
         if move < n_samples:
             self._index = (idx0, move)
@@ -135,84 +111,73 @@ class SampleManager():
         idx0 += 1
         n_full = idx0 + batch // n_samples
         for i in range(idx0, n_full):
-            if i == len(self._flops):
-                self._flops.append([])
+            if i == len(self._measurements):
+                self._measurements.append([])
 
             for j in range(n_samples):
-                self._append_flops(i, j)
+                self._append_measurement(fn, i, j)
 
         idx0 = n_full
         n_remain = batch % n_samples
         if n_remain > 0:
-            self._flops.append([])
+            self._measurements.append([])
         for j in range(n_remain):
-            self._append_flops(idx0, j)
+            self._append_measurement(fn, idx0, j)
 
         self._index = (idx0, n_remain)
 
-    def update_edfs(self) -> None:
-        """
-        Create or update the EDFs for all sampled FLOPs.
-        """
-        if self._flops is None:
-            return
-
-        if self._edfs is None:
-            self._edfs = []
-
-        for flops in self._flops[len(self._edfs):]:
-            self._edfs.append(EDF(flops))
-
-        return self._edfs
-
     def to_dict(self) -> dict[str, Any]:
         """
-        Converts this instance of ``SampleManager`` to a ``dict``. This can
+        Converts this instance of `SampleManager` to a `dict`. This can
         then be used to save and reload this instance for later use. This way
         the operations on the sampled models can be paused and later resumed.
 
         Returns:
             dict[str, Any]:
-                A ``dict`` containing the content of this manager.
+                A `dict` containing the content of this manager.
         """
         return {
             "spaces": [
                 {
                     "width_mult": space.width_mult,
                     "resolution": space.resolution,
-                    "samples": [m.to_dict() for m in lm]
+                    "samples": [model.to_dict() for model in models]
                 }
-                for space, lm in zip(self._spaces, self._models)
+                for space, models in zip(self._spaces, self._models)
             ],
-            "flops": self._flops,
+            "measurements": self._measurements,
             "index": self._index
         }
 
     @classmethod
-    def from_dict(cls, config: dict[str, Any]) -> 'SampleManager':
+    def from_dict(
+        cls,
+        config: dict[str, Any],
+        space_factory: Callable[[float, int], SearchSpace]
+    ) -> 'SampleManager':
         """
-        Converts a ``dict`` to a ``SampleManager``.
+        Converts a `dict` to a `SampleManager`.
 
         Args:
             config (dict[str, Any]):
-                The ``dict`` containing the content of a manager to be loaded.
+                The `dict` containing the content of a manager to be loaded.
 
         Returns:
             SampleManager:
-                The manager constructed from the ``dict``.
+                The manager constructed from the `dict`.
         """
-        spaces, flops, index = itemgetter("spaces", "flops", "index")(config)
-
-        models = [
-            [Model.from_dict(m) for m in s.get("samples", [])] for s in spaces
+        spaces = [
+            space_factory(entry["width_mult"], entry["resolution"])
+            for entry in config["spaces"]
         ]
-
-        spaces = [(s["width_mult"], s["resolution"]) for s in spaces]
-
         manager = cls(spaces)
-        manager._models = models
-        manager._flops = flops
-        manager._index = index
+
+        manager._models = [
+            [Model.from_dict(sample) for sample in entry.get("samples", [])]
+            for entry in config["spaces"]
+        ]
+        manager._measurements = config["measurements"]
+        manager._index = config["index"]
 
         return manager
 

d5dbc82de0d762743909b074f63ed0d1c0b4d794
Henrik Claßen <henrik.classen@web.de>
2025-02-13 15:09:27 +0100

    Add MCUNet configs

---
+++ large.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python main.py -w 0.5 -r 160 -f 2mb -s 512kb ./gtsrb ./results/large
---
+++ small.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python main.py -w 0.5 -r 160 -f 1mb -s 256kb ./gtsrb ./results/small
